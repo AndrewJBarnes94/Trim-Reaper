54a07026d4da0b74ad61ad3499c817af2f1bc393
AJ Barnes <ajbarnes@Mac.lan>
2025-04-14 12:48:51 -0400

    Started project and index.html static resource

---
+++ Trim-Reaper/src/main/java/com/brocodesoftware/trimreaper/TrimReaperApplication.java
@@ -0,0 +1,13 @@
+package com.brocodesoftware.trimreaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimReaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimReaperApplication.class, args);
+	}
+
+}
